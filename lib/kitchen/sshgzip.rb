9fc2d475c7baa311f8b1edd819725d240fae844b
Maxim Chernyak <max@bitsonnet.com>
2013-07-03 03:50:31 -0500

    Output more info

--- lib/kitchen/sshgzip.rb
+++ lib/kitchen/sshgzip.rb
@@ -10,7 +10,10 @@ module Kitchen
         gzip_path = "#{tmp_root}/#{dir}.tar.gz"
         info("Compressing #{dir} into #{dir}.tar.gz")
         system "cd #{path} && tar -zcf #{gzip_path} ."
+        upload_size = File.size(gzip_path)
+        info("Uploading #{dir}.tar.gz (#{upload_size} bytes)")
         super(scp, gzip_path, "#{dir}.tar.gz")
+        info("Unpacking #{dir}.tar.gz on the client")
         scp.session.exec! "cd #{chef_home} && mkdir #{dir} && " +
           "tar -zxf #{dir}.tar.gz -C #{dir} && rm #{dir}.tar.gz"
       else
